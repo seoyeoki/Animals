8b319b4b599447ffb9af9caa894dc2065b73426e
Kim, Seoyeon <162548811+seoyeoki@users.noreply.github.com>
2025-10-04 12:33:50 +0900

    Delete app/api-url.ts
    올려둔 API key 삭제함

--- app/api-url.ts
+++
@@ -1,3 +0,0 @@
-export const RECOMMEND_API_BASE='https://c0b3d79a49e4.ngrok-free.app'
-export const API_KEY_RECOMMEND='capstone7'
-export const API_KEY_DOGS='PVyigMpgwKQvEvlnGR9kw81jVfoq4IKM9iZME/UB0V568cajYvHyNspqQtNGWSnU/Qfk6V2J6e0j78xyXnIquA=='
